97c6ac554c04b3959576ac63e6aff6d45e89bc85
Gavin Crooks <gecrooks@users.noreply.github.com>
2025-01-22 12:47:35 -0800

    Update time_series_test.py

--- thermoflow/time_series_test.py
+++ thermoflow/time_series_test.py
@@ -173,7 +173,7 @@ def test_detect_equilibration() -> None:
 
     # Generate a time series with a long initial transient. Settles down to
     # equilibrium by about t=1300
-    ts = time_series.correlated_time_series(key, 100.0, 10000, initial=0.)
+    ts = time_series.correlated_time_series(key, 100.0, 10000, initial=10000000.)
     print(ts[0:10])
     print(ts[-10:-1])
     
